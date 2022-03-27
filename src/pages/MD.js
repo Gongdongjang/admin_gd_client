db937f3984f4af61acb43bb43af46ff29a2552f9
amado177 <69021925+amado177@users.noreply.github.com>
2022-03-28 03:46:49 +0900

    [기능추가]웹페이지 모듈화
    - react-router-dom 라이브러리를 설치하였습니다
    - Header.js는 카테고리 이동을 위해 추가하였습니다.
    - Routers.js는 페이지 라우팅 기능을 합니다
    - Nav.js는 필요 없는 페이지이나 임의로 넣어 두었습니다. 삭제하시고 MD 페이지의  {/*카테고리 별 nav*/} 를 참고하시어 추가하시면 될것같습니다.
    카테고리별 nav도 모듈화 하고싶었으나 아직 방안을 찾지 못하여 혹시 성공하시는 분있으시면 공유해주시면 감사하겠습니다.
    -각 카테고리는 임의로 추가해둔것입니다 필요시 추가,수정해서 이용하시면 됩니다

---
+++ src/pages/MD.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import React from "react";
+class MD extends React.Component{
+    render(){
+    return (
+      <div>
+       {/*카테고리 별 nav*/} 
+        <div className='side'>
+          <nav>
+            <ul>
+              <li><Link to="/md">md0</Link></li>
+              <li><Link to="/md1">md1</Link></li>
+              <li><Link to="/md2">md2</Link></li>
+            </ul>
+          </nav>
+        </div>
+        {/*페이지 내용*/} 
+        <h1>MD</h1>
+        <p>MD 페이지입니다.</p>
+      </div>
+    );
+  };
+}
+  export default MD;
