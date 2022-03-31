0f06d2796242973c2413db457bd05615bd5e4c66
amado177 <69021925+amado177@users.noreply.github.com>
2022-03-31 23:28:15 +0900

    [기능추가] MD 등록,리스트 출력

--- src/pages/Partner.js
+++ src/pages/Partner.js
@@ -3,13 +3,11 @@ import React from "react";
 class Partner extends React.Component{
     render(){
     return (
-      <div  className="section">
-      <Nav />
       <div>
-      <h1>Partner</h1>
+        <Nav />
+        <h1>Partner</h1>
         <p>Partner 페이지입니다.</p>
       </div>
-    </div>
     );
   };
 }
