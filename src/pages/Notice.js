56f8ea7cdbd3931210578b30d469d0549e51e7b7
eunjungL <dldmswjd322@gmail.com>
2023-02-01 23:06:33 +0900

    [기능추가] 공지사항 작성 CSS 추가

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -131,46 +131,40 @@ function NoticeWrite() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>
-          제목
-          <input maxLength={20} type={'text'} placeholder={'제목을 입력하세요'} name={'title'} onChange={handleChange} value={title}/>
-        </label>
+      <div className={"Notice-content"}>
+        <form className={"Notice-inputPlace"} onSubmit={handleSubmit}>
+          <div>
+            <p className={"Notice-inputTitle"}>제목</p>
+            <input className={"Notice-input"} maxLength={20} type={'text'} placeholder={'제목을 입력하세요'} name={'title'} onChange={handleChange} value={title}/>
+          </div>
+          <div onChange={handleChange}>
+            <p className={"Notice-inputTitle"}>발송 유형</p>
+            <label className={"Notice-inputTitle"}>
+              <input type={'radio'} value={'실시간'} name={'type'}/>
+              실시간
+            </label>
+            <label className={"Notice-inputTitle"}>
+              <input type={'radio'} value={'예약'} name={'type'}/>
+              예약
+            </label>
+          </div>
+          <div>
+            <p className={"Notice-inputTitle"}>업로드 날짜</p>
+            <input className={"Notice-input"} type={'datetime-local'} name={'date'} onChange={handleChange} value={date}/>
+          </div>
+          <div>
+            <p className={"Notice-inputTitle"}>이미지</p>
+            <input type={'file'} name={'photo'} onChange={handleFileChange}/>
+          </div>
+          <div>
+            <p className={"Notice-inputTitle"}>공지 텍스트</p>
+            <textarea className={"Notice-input"} maxLength={1000} placeholder={'추가 텍스트를 입력하세요.'} onChange={handleChange} name={'context'} value={context}/>
+          </div>
+          <div className={"Notice-btnPlace"}>
+            <input className={"Notice-writeBtn"} type={'submit'} value={'발송 하기'} />
+          </div>
+        </form>
       </div>
-      <div>
-        <label>
-          업로드 날짜
-          <input type={'datetime-local'} name={'date'} onChange={handleChange} value={date}/>
-        </label>
-      </div>
-      <div onChange={handleChange}>
-        <label>
-          발송 유형
-          <label>
-            <input type={'radio'} value={'실시간'} name={'type'}/>
-            실시간
-          </label>
-          <label>
-            <input type={'radio'} value={'예약'} name={'type'}/>
-            예약
-          </label>
-        </label>
-      </div>
-      <div>
-        <label>
-          이미지
-          <input type={'file'} name={'photo'} onChange={handleFileChange}/>
-        </label>
-      </div>
-      <div>
-        <label>
-          공지 텍스트
-          <textarea maxLength={1000} placeholder={'추가 텍스트를 입력하세요.'} onChange={handleChange} name={'context'} value={context}/>
-        </label>
-      </div>
-      <input type={'submit'} value={'발송 하기'} />
-    </form>
   )
 }
 
