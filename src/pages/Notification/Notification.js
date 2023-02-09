eda32ee30891ef6d92bc6cf2f85e39e5c4a47fef
eunjungL <dldmswjd322@gmail.com>
2023-02-09 16:29:50 +0900

    [기능추가] 알림 상단바 active 상태 색 바꾸게 변경

--- src/pages/Notification/Notification.js
+++ src/pages/Notification/Notification.js
@@ -2,12 +2,13 @@ import {NavLink, Route, Routes} from "react-router-dom";
 import NotificationList from "./NotificationList";
 import NotificationDetail from "./NotificationDetail";
 import NotificationWrite from "./NotificationWrite";
+import '../../CSS/Notification.css';
 
 function Notification() {
     return (
         <div>
-            <NavLink to={'/notification'}>알림 내역</NavLink>
-            <NavLink to={'/notification/write'}>알림 작성하기</NavLink>
+            <NavLink className={"Notification-menuBtn"} to={'/notification'} end>알림 내역</NavLink>
+            <NavLink className={"Notification-menuBtn"} to={'/notification/write'}>알림 작성하기</NavLink>
             <Routes>
                 <Route path={"/"} element={<NotificationList />}/>
                 <Route path={'/:notificationId'} element={<NotificationDetail />} />
