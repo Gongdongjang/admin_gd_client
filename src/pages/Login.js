f30f8f245cd11db092280d3ee7b1cdcd479d0466
eunjungL <dldmswjd322@gmail.com>
2023-02-15 16:03:58 +0900

    [리팩토링] 로그인 코드 함수형 컴포넌트로 변경

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,57 @@
-import React from "react";
-class Login extends React.Component{
-    render(){
+import React, {useState} from "react";
+import axios from "axios";
+
+function Login () {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const res = await axios.post('/api/login', {
+            id: id,
+            password: password
+        });
+
+        if (res.data.access_token === 'pwd_false') {
+            alert('비밀번호를 확인하세요');
+            document.location = '/login';
+        } else if (res.data.access_token === 'id_false') {
+            alert('아이디를 확인하세요');
+            document.location = '/login';
+        } else if (res.data.access_token === 'false') {
+            alert('모든 정보를 입력하세요');
+            document.location = '/login';
+        } else {
+            document.location = '/';
+        }
+    }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        switch (name) {
+            case 'id': setId(value); break;
+            default: setPassword(value);
+        }
+    }
+
     return (
-      <div>
-        <h1>login</h1>
-        <p>login 페이지입니다.</p>
-      </div>
-    );
-  };
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    ID
+                    <input type="text" name="id" value={id || ''} onChange={handleChange} />
+                </label>
+                <label>
+                    PASSWORD
+                    <input type="text" name="password" value={password || ''} onChange={handleChange} />
+                </label>
+                <input type='submit' value='제출' />
+            </form>
+        </div>
+    )
 }
-  export default Login;
+
+export default Login;
