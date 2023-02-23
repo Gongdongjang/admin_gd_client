1ac5217b55edb33446a08964e96ff6bf8e2ca930
eunjungL <62099953+eunjungL@users.noreply.github.com>
2023-02-24 00:36:55 +0900

    Revert "로그인 기능 완성"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { useState, useEffect }  from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './pages/Header';
-import Routers from './pages/Routers';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Routers/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
