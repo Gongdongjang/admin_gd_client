e8f6f1ed433ff9d185a980af98e53a9958b2e4e7
eunjungL <dldmswjd322@gmail.com>
2023-03-10 01:03:56 +0900

    [기능추가] 미로그인 시 접근 불가 기능 추가

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,8 +2,18 @@ import React, { useState, useEffect }  from 'react';
 
 import Header from './Header';
 import Routers from './Routers';
+import Cookies from "universal-cookie";
 
-function main() {
+function Main() {
+    const cookies = new Cookies();
+
+    useEffect(() => {
+        // 미로그인 시 튕기게 설정
+        if (cookies.get('access_token') === undefined) {
+            alert('로그인 후 사용해주세요.');
+            document.location.replace('/login');
+        }
+    }, [])
 
   return (
     <div>
@@ -13,4 +23,4 @@ function main() {
   );
 }
 
-export default main;
+export default Main;
