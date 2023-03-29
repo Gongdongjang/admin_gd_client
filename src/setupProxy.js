c1fd8d0b391d647bb1c820213e26d122f6dbb629
eunjungL <dldmswjd322@gmail.com>
2023-03-30 01:44:28 +0900

    [기능수정] setUpProxy 프로덕션으로 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
     app.use(
         createProxyMiddleware('/api', {
-            target: 'http://localhost:5000/',
+            target: 'http://3.37.223.12:5000/',
             changeOrigin: true
         })
     )
